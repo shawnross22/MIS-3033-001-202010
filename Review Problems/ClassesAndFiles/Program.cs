3a470dfc4965ae541d68187ad76a62ec51d13ae3
Shawn Ross <Shawn.M.Ross-1@ou.edu>
2020-09-01 18:02:50 -0500

    Completed Student UML and Date Time Functions

--- Review Problems/ClassesAndFiles/Program.cs
+++ Review Problems/ClassesAndFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesAndFiles
 {
@@ -6,7 +7,37 @@ namespace ClassesAndFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+            Student stud1 = new Student();
+            stud1.setName("Shawn");
+            stud1.id = 22;
+            stud1.name = "Shawn";
+            students.Add(stud1);
+
+            Student stud2 = new Student("Shawn", 22);
+            students.Add(stud2);
+
+            Student stud3 = new Student("John", 1);
+            students.Add(stud3);
+
+            foreach (var stud in students)
+            {
+                
+                Console.WriteLine(stud);
+            }
+
+            DateTime current = DateTime.Now;
+            DateTime past = Convert.ToDateTime("4/7/1999");
+
+            var result = current - past;
+            Console.WriteLine($"It has been {result.TotalDays} since my birthday");
+
+            TimeSpan difference = current - stud1.enrolldate;
+            Console.WriteLine($" It has been {difference.TotalMilliseconds}ms since {stud1.name} has enrolled");
+
+            Console.WriteLine($"There are currently {Student.totalStudents} enrolled"); 
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Review Problems/ClassesAndFiles/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndFiles
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public DateTime enrolldate { get; set; }
+        public static int totalStudents { get; set; }
+
+        public Student (string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+            enrolldate = DateTime.Now;
+            totalStudents++;
+        }
+
+        public Student()
+        {
+            name = string.Empty;
+            id = -1;
+            enrolldate = DateTime.Now;
+            totalStudents++;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} has an id of {id}";
+        }
+    }
+}
