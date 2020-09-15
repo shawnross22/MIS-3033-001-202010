53ea0ccc7ed742fc6c9e9c1592b602ba4b7e7084
Shawn Ross <Shawn.M.Ross-1@ou.edu>
2020-09-15 18:00:54 -0500

    Completed the CSV example project

---
+++ In-Class Examples/CSV Example/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSV_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filepath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnValidate_Click(object sender, RoutedEventArgs e)
+        {
+            filepath = txtFilePath.Text;
+
+            if (File.Exists(filepath) == true)
+            {
+                btnProcess.IsEnabled = true;
+                btnValidate.IsEnabled = false;
+                txtFilePath.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Invalid file path. Please try agian.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtFilePath.Clear();
+                txtFilePath.Focus(); //puts cursor back in textbox
+            }
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+
+            double sum = 0;
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                var pieces = line.Split(',');
+
+                double price;
+
+                if (Double.TryParse(pieces[2], out price) == true)
+                {
+                    sum += Convert.ToDouble(pieces[2]);
+                }
+                else
+                {
+                    MessageBox.Show($"Sorry, there was an invalid price on line {i+1}.");
+                }
+
+                lstFile.Items.Add(pieces[1]);
+            }
+            MessageBox.Show($"The sum of all the product prices is {sum.ToString("C2")}.");
+
+
+               
+            
+        }
+    }
+}
---
+++ Participations/First WPF Application/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace First_WPF_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtName.Text = String.Empty;
+            txtBday.Text = String.Empty;
+            btnCalculateAge.IsEnabled = true;
+
+
+        }
+
+        private void btnCalculateAge_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string name = txtName.Text;
+            string bday = txtBday.Text;
+
+            DateTime bdaydatetime = Convert.ToDateTime(bday);
+
+            int age = Convert.ToInt32((DateTime.Now - bdaydatetime).TotalDays);
+
+            int ageinyears = age / 365;
+
+            string message = $"{name}, you are {ageinyears} years old.";
+
+            MessageBox.Show(message);
+
+
+
+
+            
+        }
+
+        private void btnCalculateAge_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnCalculateAge.Background = Brushes.DeepPink;
+        }
+
+        private void btnCalculateAge_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnCalculateAge.Background = Brushes.OrangeRed;
+        }
+
+        private void btnCalculateAge_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
