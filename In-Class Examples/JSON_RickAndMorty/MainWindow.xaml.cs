eba49d5ebe2640008478a93efe31eca31bd905f9
Shawn Ross <Shawn.M.Ross-1@ou.edu>
2020-09-27 13:41:49 -0500

    worked on RickAndMortyAPI

---
+++ In-Class Examples/JSON_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = "https://rickandmortyapi.com/api/character/";
+            RickAndMortyAPI api;
+            using (var client = new HttpClient())
+            {
+                string results = client.GetStringAsync(url).Result;
+
+                api = JsonConvert.DeserializeObject<RickAndMortyAPI>(results);
+
+            }
+
+            foreach (var character in api.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character selectedCharacter = (Character)cboCharacters.SelectedItem;
+
+            Uri uri = new Uri(selectedCharacter.image);
+            BitmapImage picture = new BitmapImage(uri);
+
+            imgPicture.Source = picture;
+            lblCharacterInfo.Content = $"{selectedCharacter.name}";
+        }
+    }
+}
---
+++ In-Class Examples/JSON_RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_RickAndMorty
+{
+    class RickAndMortyAPI
+    {
+        public Info info { get; set; }
+
+        public List<Character> results { get; set; }
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+}
